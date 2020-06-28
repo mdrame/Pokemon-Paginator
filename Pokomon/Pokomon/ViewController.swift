9e04018bda3cffac086cf66717f21dad14857bb7
MO <mo@MOs-iMac.home>
2020-06-28 02:12:26 -0400

    start project

---
+++ Pokomon/Pokomon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokomon
+//
+//  Created by MO on 6/28/20.
+//  Copyright © 2020 MO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
